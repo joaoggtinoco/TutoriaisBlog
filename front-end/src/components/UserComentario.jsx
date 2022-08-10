73b97356298046664f91222cbc6d2417cccaed5d
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2022-08-09 22:16:53 -0300

    Limpando o codigo front end

--- front-end/src/components/UserComentario.jsx
+++ front-end/src/components/UserComentario.jsx
@@ -1,71 +1,34 @@
-import { gql, useMutation } from '@apollo/client'
 import React from 'react'
-import { useEffect } from 'react'
 import UserIcon from './iconeUser/UserIcon'
 import Loading from './loading/Loading'
 import './userComentario.css'
-import axios from "axios"
-
-const CREATE_USER_POST = gql`
-  mutation CreateUserPost($description: String!) {
-    createUserPost(data: { description: $description }) {
-      id
-    }
-  }
-`
+import axios from 'axios'
 
 function UserComentario() {
-
   const [description, setDescription] = React.useState('')
-  
-  /*
-  useEffect(() => {
-    axios.post("https://localhost:7276/api/Postagem"), {
-      conteudo: description,
-      usuarioId: 8 
-    }
-      .then(() => {
-        alert("Post enviado")
-      })
-      .catch(() =>{
-        alert("Deu erro")
-      })
-  });
-*/
-
-
-
-
-  const [createPost, {loading}] = useMutation(CREATE_USER_POST);
+  const [loading, setLoading] = React.useState(false)
 
   // So pra ver o texto quando clicar no botao enviar
   const [submitComment, setSubmitComment] = React.useState('')
 
   function handleSubmitPost(event) {
     event.preventDefault()
+    setLoading(true)
     setSubmitComment(description)
-    axios.post("https://localhost:7276/api/Postagem", {
-      conteudo: description,
-      usuarioId: 8 
-    })
+    axios
+      .post('https://localhost:7276/api/Postagem', {
+        conteudo: description,
+        usuarioId: 8
+      })
       .then(() => {
-        alert("Post enviado")
+        alert('Post enviado')
+        setLoading(false)
       })
-      .catch(() =>{
-        alert("Deu erro")
+      .catch(() => {
+        alert('Deu erro')
+        setLoading(false)
       })
-  
 
-    // Fazer req para api enviando o dado para o banco
-    createPost({
-      variables:{
-        // Essa variavel de estado tem que ter o mesmo nome na req da mutation
-        description
-      }
-    })
-
-    console.log(`Toke: ${process.env.REACT_APP_API_ACCESS_TOKEN}`)
-    console.log(`Base URL: ${process.env.REACT_APP_URI}`)
   }
   return (
     <div className="container-main">
@@ -83,10 +46,7 @@ function UserComentario() {
         ></textarea>
 
         <footer>
-          <button
-            type="submit"
-            disabled={description.length === 0 || loading}
-          >
+          <button type="submit" disabled={description.length === 0 || loading}>
             {loading ? <Loading /> : 'Enviar Postagem'}
           </button>
         </footer>
