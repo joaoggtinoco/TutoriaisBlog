10981917db3fa96d8ccb19056e212965bdbbd087
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2022-08-15 12:05:59 -0300

    Melhorando a tela de cadastro e login 🚀

--- front-end/src/pages/Subscribe.jsx
+++ front-end/src/pages/Subscribe.jsx
@@ -3,6 +3,8 @@ import Logo from '../assets/LogoNomeLongo.svg'
 import Footer from '../components/Footer'
 import LoadingDeafult from '../components/loading/LoadingDefault'
 
+import { Eye, EyeClosed } from 'phosphor-react'
+
 import React from 'react'
 
 import { useNavigate } from 'react-router-dom'
@@ -11,6 +13,7 @@ export default function Subscribe({ type }) {
   const navigate = useNavigate()
 
   const [loading, setLoading] = React.useState(false)
+  const [eye, setEye] = React.useState(false)
   const [email, setEmail] = React.useState('')
   const [password, setPassword] = React.useState('')
   const [name, setName] = React.useState('')
@@ -45,14 +48,14 @@ export default function Subscribe({ type }) {
   }
 
   return (
-    <div className='h-[100vh] flex flex-col justify-between'>
+    <div className="h-[100vh] flex flex-col justify-between">
       <Header type="default" />
       {type === 'login' ? (
         <div className="flex flex-col items-center gap-[1.313rem] pt-[9.2rem] pb-7 px-6 h-[100vh]">
           <img src={Logo} alt="logo" />
           <form
             onSubmit={handleLoginUser}
-            className="flex flex-col items-center justify-between gap-[1.85rem] bg-zinc-500 rounded-[8px] p-5 w-full lg:max-w-[50%] h-[100vh]"
+            className="flex flex-col items-center justify-between gap-[1.85rem] bg-zinc-500 rounded-[8px] p-5 w-full lg:max-w-[72.5rem] h-[100vh]"
           >
             <div className="flex flex-col w-full items-center gap-5">
               <label
@@ -67,27 +70,49 @@ export default function Subscribe({ type }) {
                 name="Email"
                 placeholder="Digite o seu Email"
                 onChange={event => setEmail(event.target.value)}
-                className="bg-zinc-300 rounded-[5px] px-5 h-20 w-full lg:max-w-[50%] placeholder-black placeholder:text-[15px] 
+                className="bg-zinc-300 rounded-[5px] px-5 h-20 w-full lg:max-w-lg placeholder-black placeholder:text-[15px] 
               text-[15px] outline-0
               focus:outline-none focus:border focus:ring-green-300 focus:border-green-300 hover:border-green-300 hover:border invalid:text-red-500 focus:invalid:border-red-500 transition-all duration-500 ease-in-out"
                 required
               />
+
               <label
                 for="Password"
                 className="text-orange-300 font-bold text-[2.25rem]"
               >
                 Senha
               </label>
-              <input
-                type="password"
-                id="Password"
-                name="Password"
-                placeholder="Digite sua senha"
-                onChange={event => setPassword(event.target.value)}
-                className="bg-zinc-300 rounded-[5px] px-5 h-20 w-full lg:max-w-[50%] placeholder-black placeholder:text-[15px] text-[15px] outline-0
-              focus:outline-none focus:border focus:ring-green-300 focus:border-green-300 hover:border-green-300 hover:border invalid:text-red-500 focus:invalid:border-red-500 transition-all duration-500 ease-in-out lg:text-[100%]"
-                required
-              />
+              <div className="flex flex-col w-full items-center gap-5 relative">
+                <input
+                  type={eye ? 'text' : 'password'}
+                  id="Password"
+                  name="Password"
+                  placeholder="Digite sua senha maior/igual a 5"
+                  onChange={event => setPassword(event.target.value)}
+                  className={`bg-zinc-300 rounded-[5px] px-5 h-20 w-full lg:max-w-lg placeholder-black placeholder:text-[15px] text-[15px] outline-0
+              focus:outline-none focus:border focus:ring-green-300 focus:border-green-300 hover:border-green-300 hover:border invalid:text-red-500 focus:invalid:border-red-500 ${
+                password.length < 5 ? 'focus:border-red-500 text-red-500' : ''
+              } transition-all duration-500 ease-in-out`}
+                  required
+                />
+                <button
+                  className="absolute top-0 bottom-0 right-0 lg:right-80 pr-3 lg:pr-0 cursor-pointer"
+                  onClick={event => {
+                    event.preventDefault()
+                    eye ? setEye(false) : setEye(true)
+                  }}
+                >
+                  {eye ? <Eye size={30} /> : <EyeClosed size={30} />}
+                </button>
+              </div>
+              <button
+                className="text-orange-300 hover:hover:text-orange-500 transition-colors font-bold text-[1.5rem]"
+                onClick={() => {
+                  navigate('/cadastrar')
+                }}
+              >
+                Esqueceu sua senha?
+              </button>
             </div>
             <div className="flex flex-col items-center gap-8 w-full">
               <button
@@ -101,7 +126,7 @@ export default function Subscribe({ type }) {
                 onClick={() => {
                   navigate('/cadastrar')
                 }}
-                className="text-white font-bold lg:text-[2rem] text-[17px] px-3 py-5 lg:py-5 bg-orange-300 flex justify-center first-letter w-auto sm:w-[40%] rounded-[5px] outline-none hover:bg-orange-500 focus:bg-orange-500 transition-colors disabled:opacity-50"
+                className="text-white font-bold lg:text-[2rem] text-[17px] px-3 py-5 lg:py-5 bg-orange-300 flex justify-center first-letter w-auto sm:w-[40%] rounded-[5px] outline-none hover:bg-orange-500 focus:bg-orange-500 transition-colors"
               >
                 Não tem uma conta?
               </button>
@@ -114,7 +139,7 @@ export default function Subscribe({ type }) {
             <img src={Logo} alt="logo" />
             <form
               onSubmit={handleCreateUser}
-              className="flex flex-col items-center justify-between gap-[1.85rem] bg-zinc-500 rounded-[8px] p-5 w-full lg:max-w-[50%]"
+              className="flex flex-col items-center justify-between gap-[1.85rem] bg-zinc-500 rounded-[8px] p-5 w-full lg:max-w-[72.5rem]"
             >
               <div className="flex flex-col w-full items-center gap-5">
                 <label
@@ -127,10 +152,12 @@ export default function Subscribe({ type }) {
                   type="text"
                   id="User"
                   name="User"
-                  placeholder="Digite seu nome"
+                  placeholder="Digite seu nome maior/igual a 3"
                   onChange={event => setName(event.target.value)}
-                  className="bg-zinc-300 rounded-[5px] px-5 h-20 w-full lg:max-w-[50%] placeholder-black placeholder:text-[15px]  text-[15px] outline-0
-              focus:outline-none focus:border focus:ring-green-300 focus:border-green-300 hover:border-green-300 hover:border invalid:text-red-500 focus:invalid:border-red-500 transition-all duration-500 ease-in-out"
+                  className={`bg-zinc-300 rounded-[5px] px-5 h-20 w-full lg:max-w-lg placeholder-black placeholder:text-[15px]  text-[15px] outline-0
+              focus:outline-none focus:border focus:ring-green-300 focus:border-green-300 hover:border-green-300 hover:border invalid:text-red-500 focus:invalid:border-red-500 transition-all duration-500 ease-in-out ${
+                name.length < 3 ? 'focus:border-red-500 text-red-500' : ''
+              }`}
                   required
                 />
                 <label
@@ -145,7 +172,7 @@ export default function Subscribe({ type }) {
                   name="Email"
                   placeholder="Digite o seu Email"
                   onChange={event => setEmail(event.target.value)}
-                  className="bg-zinc-300 rounded-[5px] px-5 h-20 w-full lg:max-w-[50%] placeholder-black placeholder:text-[15px] 
+                  className="bg-zinc-300 rounded-[5px] px-5 h-20 w-full lg:max-w-lg placeholder-black placeholder:text-[15px] 
               text-[15px] outline-0
               focus:outline-none focus:border focus:ring-green-300 focus:border-green-300 hover:border-green-300 hover:border invalid:text-red-500 focus:invalid:border-red-500 transition-all duration-500 ease-in-out"
                   required
@@ -156,26 +183,39 @@ export default function Subscribe({ type }) {
                 >
                   Senha
                 </label>
-                <input
-                  type="password"
-                  id="Password"
-                  name="Password"
-                  placeholder="Digite sua senha"
-                  onChange={event => setPassword(event.target.value)}
-                  className="bg-zinc-300 rounded-[5px] px-5 h-20 w-full lg:max-w-[50%] placeholder-black placeholder:text-[15px] text-[15px] outline-0
-              focus:outline-none focus:border focus:ring-green-300 focus:border-green-300 hover:border-green-300 hover:border invalid:text-red-500 focus:invalid:border-red-500 transition-all duration-500 ease-in-out"
-                  required
-                />
+                <div className="flex flex-col w-full items-center gap-5 relative">
+                  <input
+                    type={eye ? 'text' : 'password'}
+                    id="Password"
+                    name="Password"
+                    placeholder="Digite sua senha maior/igual a 5"
+                    onChange={event => setPassword(event.target.value)}
+                    className={`bg-zinc-300 rounded-[5px] px-5 h-20 w-full lg:max-w-lg placeholder-black placeholder:text-[15px] text-[15px] outline-0
+              focus:outline-none focus:border focus:ring-green-300 focus:border-green-300 hover:border-green-300 hover:border invalid:text-red-500 focus:invalid:border-red-500 ${
+                password.length < 5 ? 'focus:border-red-500 text-red-500' : ''
+              } transition-all duration-500 ease-in-out`}
+                    required
+                  />
+                  <button
+                    className="absolute top-0 bottom-0 right-0 lg:right-80 pr-3 lg:pr-0 cursor-pointer"
+                    onClick={event => {
+                      event.preventDefault()
+                      eye ? setEye(false) : setEye(true)
+                    }}
+                  >
+                    {eye ? <Eye size={30} /> : <EyeClosed size={30} />}
+                  </button>
+                </div>
               </div>
               <div className="flex flex-col items-center gap-8 w-full">
                 <button
                   disabled={
                     loading ||
-                    email.length < 5 ||
+                    email.length === 0 ||
                     password.length < 5 ||
                     name.length < 3
                   }
-                  className="text-white font-bold lg:text-[2rem] text-[17px] px-3 py-5 lg:py-5 bg-orange-300 flex justify-center first-letter w-auto sm:w-[35%] rounded-[5px] outline-none hover:bg-orange-500 focus:bg-orange-500 transition-colors disabled:opacity-50"
+                  className="text-white font-bold lg:text-[2rem] text-[17px] px-3 py-5 lg:py-5 bg-orange-300 flex justify-center first-letter w-auto sm:w-[35%] rounded-[5px] outline-none hover:bg-orange-500 focus:bg-orange-500 transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
                   type="submit"
                 >
                   {loading ? <LoadingDeafult /> : 'Finalizar cadastro'}
@@ -184,7 +224,7 @@ export default function Subscribe({ type }) {
                   onClick={() => {
                     navigate('/')
                   }}
-                  className="text-white font-bold lg:text-[2rem] text-[17px] px-3 py-5 lg:py-5 bg-orange-300 flex justify-center first-letter w-auto sm:w-[40%] rounded-[5px] outline-none hover:bg-orange-500 focus:bg-orange-500 transition-colors disabled:opacity-50"
+                  className="text-white font-bold lg:text-[2rem] text-[17px] px-3 py-5 lg:py-5 bg-orange-300 flex justify-center first-letter w-auto sm:w-[40%] rounded-[5px] outline-none hover:bg-orange-500 focus:bg-orange-500 transition-colors"
                 >
                   Já tem uma conta?
                 </button>
