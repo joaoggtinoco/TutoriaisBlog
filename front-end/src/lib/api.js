08a893355a8251d77f15471044979d33e96a7522
joaoggtinoco <joaoggtinocodev@gmail.com>
2022-08-26 21:01:56 -0300

    Correção da URL da API

--- front-end/src/lib/api.js
+++ front-end/src/lib/api.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
   // o endereço base do nosso back-end
-  baseURL: 'https://localhost:7276'
+  baseURL: 'http://54.207.212.51:7276'
 })
